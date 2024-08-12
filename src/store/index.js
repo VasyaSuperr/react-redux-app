f71a8d37e3889e0ea41f4c8316d518d458c1156a
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-08-12 13:49:59 +0300

    add mapDispatchToProps

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slices/counterSlice";
+import themeReduser from "./slices/themeSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    themeData: themeReduser,
   },
 });
 
@@ -12,27 +14,6 @@ export default store;
 // reducer - чиста функція
 // action - js-object з обов'язковою властивістю
 //          {type: "", payload: }
-
-// const initialState = {
-//     count: 0,
-//     step: 1,
-//   };
-//   function reducer(state = initialState, action) {
-//     const { type } = action;
-//     const { count, step } = state;
-//     switch (type) {
-//       case "decrement":
-//         return { ...state, count: count - step };
-//       case "increment":
-//         return { ...state, count: count + step };
-//       case "setStep":
-//         const { payload } = action;
-//         return { ...state, step: payload };
-
-//       default:
-//         return state;
-//     }
-//   }
 
 //   // сховище = стан + як змінювати стан (reduser)
 //   const store = legacy_createStore(reducer);
